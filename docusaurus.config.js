1094bc9e56fbca1d73924f61e3f796625dc455cc
yumiharu-nakano <nakano.tokyotech@gmail.com>
2026-04-11 17:54:52 +0900

    ページ編集

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Yumiharu Nakano's Lab. Built with Docusaurus.`,
+        //copyright: `Copyright © ${new Date().getFullYear()} Yumiharu Nakano's Lab. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
